543a53d9e7a35ea34688aa11b5cab56095070599
mohankumar <mohankumar.kandhasamy@gmail.com>
2020-02-15 01:01:43 +0530

    initial commit

--- src/main/java/com/mohan/security/service/impl/UserServiceImpl.java
+++ src/main/java/com/mohan/security/service/impl/UserServiceImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.mohan.security.dao.UserDao;
-import com.mohan.security.model.User;
+import com.mohan.security.model.User; 
 import com.mohan.security.service.UserService;
 
 @Service
